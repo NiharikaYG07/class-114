e4552b7f6a206821c361cddfd2a926297ee62301
NiharikaYG07 <114799693+NiharikaYG07@users.noreply.github.com>
2023-06-20 15:11:11 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+a=33
+b=int('33')
+if a>b:
+    print("a is greater than b")
+elif b==a:
+    print("a and b are equal")
+
+def myFunction(value):
+    print(value+"refsnes")
+
+myFunction("email")
+myFunction("linux")
+myFunction("tobias")
